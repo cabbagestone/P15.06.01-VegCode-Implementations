1a2cc795d7177418f3ee4c5800e7a4ec29bbe99d
Cabbage Stone <calebstone800@gmail.com>
2024-04-09 15:43:41 -0600

    algorithmically complete rust implementation - albeit a bit ugly

--- rust-vegcode/src/main.rs
+++ rust-vegcode/src/main.rs
@@ -1,38 +1,38 @@
-use chrono::prelude::*;
+use chrono::{prelude::*, Duration};
 
 fn main() {
-    print!("{}", to_vercode());
+    from_vegcode(to_vegcode());
 }
 
-fn to_vercode() -> String {
-    let vercode_version = String::from("0");
+fn to_vegcode() -> String {
+    let vegcode_version = String::from("0");
 
-    let flag_author = false;
-    let flag_draft = false;
+    let flag_author = true;
+    let flag_draft = true;
     let flag_archive = false;
 
     let mut flag_v2_4 = false;
 
-    let sacsi_id = String::from("Z99.99.99");
-    let v2_version = String::from("99.99.99");
+    let sacsi_id = String::from("J31.01.02");
+    let v2_version = String::from("04.03.00");
     let created_date = String::from("2024-04-08");
     let last_updated_date = String::from("2024-04-09");
 
-    let mut vercode = String::from("0");
+    let mut vegcode = String::new();
     let mut flags = String::from("000000");
     let mut v2_base62 = String::new();
 
-    set_binary_string_bit(&mut flags, 0, flag_author);
-    set_binary_string_bit(&mut flags, 1, flag_draft);
-    set_binary_string_bit(&mut flags, 2, flag_archive);
+    set_binary_string_bit(&mut flags, 5, flag_author);
+    set_binary_string_bit(&mut flags, 4, flag_draft);
+    set_binary_string_bit(&mut flags, 3, flag_archive);
 
-    let flag_unversioned_file = v2_version.len() == 0;
+    let flag_versioned_file = v2_version.len() != 0;
 
-    if flag_unversioned_file && last_updated_date.len() > 0 {
+    if !flag_versioned_file && last_updated_date.len() > 0 {
         panic!("Modified date is set on an unversioned file");
     }
 
-    if !flag_unversioned_file {
+    if flag_versioned_file {
         v2_base62 = base10_to_base62(convert_period_separated_string_to_base10(
             v2_version.clone(),
         ));
@@ -41,9 +41,9 @@ fn to_vercode() -> String {
     let sacsi_base62 = base10_to_base62(sacsi_id_to_base10(sacsi_id.clone()));
     let flag_sacsi_5 = sacsi_base62.len() == 5;
 
-    set_binary_string_bit(&mut flags, 3, flag_sacsi_5);
-    set_binary_string_bit(&mut flags, 4, flag_v2_4);
-    set_binary_string_bit(&mut flags, 5, flag_unversioned_file);
+    set_binary_string_bit(&mut flags, 2, flag_sacsi_5);
+    set_binary_string_bit(&mut flags, 1, flag_v2_4);
+    set_binary_string_bit(&mut flags, 0, flag_versioned_file);
 
     let flags_base64 = base2_to_friendly64(flags);
 
@@ -66,72 +66,108 @@ fn to_vercode() -> String {
     let last_updated_date_base62 =
         base10_to_base62(days_between_updated_datetime_and_created_datetime as u64);
 
-    vercode.push_str(&vercode_version);
-    vercode.push_str(&flags_base64);
-    vercode.push_str(&sacsi_base62);
+    vegcode.push_str(&vegcode_version);
+    vegcode.push_str(&flags_base64);
+    vegcode.push_str(&sacsi_base62);
 
-    if !flag_unversioned_file {
-        vercode.push_str(&v2_base62);
-        vercode.push_str(&created_date_base62);
-        vercode.push_str(&last_updated_date_base62);
+    if flag_versioned_file {
+        vegcode.push_str(&v2_base62);
+        vegcode.push_str(&created_date_base62);
+        vegcode.push_str(&last_updated_date_base62);
     } else {
-        vercode.push_str(&created_date_base62);
+        vegcode.push_str(&created_date_base62);
     }
 
-    vercode
+    vegcode
 }
 
-fn from_vercode(vercode: String) {
+fn from_vegcode(vegcode: String) {
+    let mut vegcode_iterator = vegcode.chars();
 
+    let vegcode_version = vegcode_iterator.next().unwrap().to_string();
+    let flags_base64 = vegcode_iterator.next().unwrap().to_string();
 
-    let vercode_version = vercode.chars().nth(0).unwrap().to_string();
-    let flags_base64 = vercode.chars().skip(1).take(1).collect::<String>();
+    let binding = friendly64_to_binary_string(flags_base64);
+    // push zeros onto the beginning of the string until it is 6 characters long
+    let binding = format!("{:0>6}", binding);
+    let mut flags = binding.chars();
 
-    let flags = friendly64_to_binary_string(flags_base64);
-
-    let read_flag_author = flags.chars().nth(0).unwrap() == '1';
-    let read_flag_draft = flags.chars().nth(1).unwrap() == '1';
-    let read_flag_archive = flags.chars().nth(2).unwrap() == '1';
-    let read_flag_sacsi_5 = flags.chars().nth(3).unwrap() == '1';
-    let read_flag_v2_4 = flags.chars().nth(4).unwrap() == '1';
-    let read_flag_unversioned_file = flags.chars().nth(5).unwrap() == '1';
+    let flag_versioned_file = flags.next().unwrap() == '1';
+    let read_flag_v2_4 = flags.next().unwrap() == '1';
+    let read_flag_sacsi_5 = flags.next().unwrap() == '1';
+    let read_flag_archive = flags.next().unwrap() == '1';
+    let read_flag_draft = flags.next().unwrap() == '1';
+    let read_flag_author = flags.next().unwrap() == '1';
 
     let sacsi_length = if read_flag_sacsi_5 { 5 } else { 4 };
-    let v2_length = if read_flag_v2_4 { 4 } else { 0 };
+    let v2_length = if read_flag_v2_4 { 4 } else { 3 };
 
-    let sacsi_base62 = vercode
-        .chars()
-        .skip(2)
-        .take(sacsi_length)
-        .collect::<String>();
+    let vegcode_vec: Vec<char> = vegcode_iterator.collect();
 
-    let mut read_created_date_base62 = String::new();
-    let mut read_last_updated_date_base62 = String::new();
-    let mut read_v2_base62 = String::new();
+    let created_date_base62;
+    let mut v2_version = String::new();
+    let mut last_updated_date = 0;
+    let last_updated_datetime;
 
-    if read_flag_unversioned_file {
-        read_created_date_base62 = vercode
-            .chars()
-            .skip(2 + sacsi_length)
-            .take(4)
-            .collect::<String>();
+    let sacsi_base62 = vegcode_vec.iter().take(sacsi_length).collect();
+    if !flag_versioned_file {
+        created_date_base62 = vegcode_vec.iter().skip(sacsi_length).collect();
     } else {
-        read_v2_base62 = vercode
-            .chars()
-            .skip(2 + sacsi_length)
+        let v2_base62 = vegcode_vec
+            .iter()
+            .skip(sacsi_length)
             .take(v2_length)
-            .collect::<String>();
-        read_created_date_base62 = vercode
-            .chars()
-            .skip(2 + sacsi_length + v2_length)
-            .take(4)
-            .collect::<String>();
-        read_last_updated_date_base62 = vercode
-            .chars()
-            .skip(2 + sacsi_length + v2_length + 4)
-            .take(4)
-            .collect::<String>();
+            .collect();
+
+        created_date_base62 = vegcode_vec
+            .iter()
+            .skip(sacsi_length + v2_length)
+            .take(3)
+            .collect();
+
+        let last_updated_date_base62 = vegcode_vec
+            .iter()
+            .skip(sacsi_length + v2_length + 3)
+            .collect();
+
+        v2_version = base62_to_period_separated_string(v2_base62);
+        last_updated_date = base62_to_base10(last_updated_date_base62);
     }
+
+    let sacsi_id = base62_to_sacsi_id(sacsi_base62);
+    let created_date = base62_to_base10(created_date_base62);
+
+    let created_datetime = NaiveDate::from_ymd_opt(2000, 1, 1)
+        .ok_or("Invalid date")
+        .unwrap()
+        + Duration::days(created_date as i64);
+
+    let created_datetime_string = created_datetime.format("%Y-%m-%d").to_string();
+
+    let mut last_updated_datetime_string = String::new();
+    if flag_versioned_file {
+        last_updated_datetime = created_datetime + Duration::days(last_updated_date as i64);
+        last_updated_datetime_string = last_updated_datetime.format("%Y-%m-%d").to_string();
+    }
+
+    println!("Vegcode version: {}", vegcode_version);
+    println!("Author flag: {}", read_flag_author);
+    println!("Draft flag: {}", read_flag_draft);
+    println!("Archive flag: {}", read_flag_archive);
+    println!("Sacsi 5 flag: {}", read_flag_sacsi_5);
+    println!("V2 4 flag: {}", read_flag_v2_4);
+    println!("Versioned file flag: {}", flag_versioned_file);
+    println!("Sacsi ID: {}", sacsi_id);
+    println!("Created date: {}", created_datetime_string);
+    if flag_versioned_file {
+        println!("Last updated date: {}", last_updated_datetime_string);
+        println!("V2 Version: {}", v2_version);
+    }
+}
+
+fn base62_to_period_separated_string(string_base62: String) -> String {
+    let num = base62_to_base10(string_base62);
+    base10_to_period_separated_string(num)
 }
 
 fn sacsi_id_to_base10(sacsi_id: String) -> u64 {
@@ -165,8 +201,8 @@ fn base62_to_sacsi_id(string_base62: String) -> String {
 
 fn base10_to_sacsi_id(num: u64) -> String {
     let mut sacsi_id = String::new();
-    let first_char = (num / 1_000_000) + 96;
-    sacsi_id.push(first_char as u8 as char);
+    let first_char = (((num / 1_000_000) + 96) as u8 as char).to_ascii_uppercase();
+    sacsi_id.push(first_char);
     let rest_of_id = num % 1_000_000;
     sacsi_id.push_str(&base10_to_period_separated_string(rest_of_id));
     sacsi_id
@@ -178,6 +214,9 @@ fn base10_to_period_separated_string(num: u64) -> String {
     while num > 0 {
         let remainder = num % 100;
         string.push_str(&remainder.to_string());
+        if remainder < 10 {
+            string.push('0');
+        }
         string.push('.');
         num = num / 100;
     }
